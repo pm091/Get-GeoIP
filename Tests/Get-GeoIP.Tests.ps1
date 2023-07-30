3719932a102127c225d220bafc7d9e6af1573838
PM_091 <PM_091@outlook.com>
2023-07-30 16:31:20 +0100

    Deploy!

--- Tests/Get-GeoIP.Tests.ps1
+++ Tests/Get-GeoIP.Tests.ps1
@@ -2,12 +2,12 @@ Describe "Get-GeoIP PS$PSVersion Unit tests" {
 
    It 'Should Return Google LLC' {
     $Output = Get-GeoIP 8.8.8.8
-    $Output.ISP | Should be 'Google LLC'
+    $Output.ISP | Should -Be 'Google LLC'
    }
 
    It 'Should Return Google LLC' {
     $Output = Get-GeoIP 8.8.4.4
-    $Output.ISP | Should be 'Google LLC'
+    $Output.ISP | Should -Be 'Google LLC'
    }
 }
 
