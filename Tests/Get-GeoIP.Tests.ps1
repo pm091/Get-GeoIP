ca5cd4ba4c12d20bc80a0e1d1975823781b96be7
pm091 <pm_091@outlook.com>
2025-07-31 21:29:24 +0100

    Update Get-GeoIP.Tests.ps1
    Update Tests

--- Tests/Get-GeoIP.Tests.ps1
+++ Tests/Get-GeoIP.Tests.ps1
@@ -9,5 +9,15 @@ Describe "Get-GeoIP PS$PSVersion Unit tests" {
     $Output = Get-GeoIP 8.8.4.4
     $Output.ISP | Should -Be 'Google LLC'
    }
+
+   It 'Should Return Google LLC' {
+    $Output = Get-GeoIP 2001:4860:4860::8888
+    $Output.ISP | Should -Be 'Google LLC'
+   }
+
+   It 'Should Return Google LLC' {
+    $Output = Get-GeoIP 2001:4860:4860::8844
+    $Output.ISP | Should -Be 'Google LLC'
+   }
 }
 
